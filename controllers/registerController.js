ec3abfb5308cd6f43c38569d87391fc7b72cbdad
malekbouzaabia <malek23bouzaabia@gmail.com>
2026-06-01 22:51:01 +0100

    emaile1

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,7 +1,7 @@
 const Auditeur   = require('../models/Auditeur')
 const bcrypt     = require('bcryptjs')
 const { Resend } = require('resend')
-const resend = new Resend(process.env.RESEND_API_KEY)
+const nodemailer = require('nodemailer')
 
 // ── Stockage temporaire avant vérification ───────────────────
 const pendingUsers = new Map()
@@ -17,17 +17,29 @@ setInterval(() => {
 }, 15 * 60 * 1000)
 
 // ── Transporter Email ─────────────────────────────────────────
+const transporter = nodemailer.createTransport({
+  host: '74.125.133.108',
+  port: 587,
+  family: 4,
+  secure:  false,
+  auth: {
+    user: process.env.MAIL_USER || '',
+    pass: process.env.MAIL_PASS || '',
+  },
+  tls: { rejectUnauthorized: false, servername: 'smtp.gmail.com' },
+  family: 4,
+})
 
 async function sendEmail(to, subject, html) {
   try {
     console.log('📧 Envoi email a:', to)
-    await resend.emails.send({
-      from: 'AuditWise <onboarding@resend.dev>',
-      to,
-      subject,
-      html,
-    })
-    console.log('✅ Email envoyé à:', to)
+   await resend.emails.send({
+  from: 'AuditWise <onboarding@resend.dev>',
+  to,
+  subject,
+  html,
+})
+    console.log('✅ Email envoye avec succes a:', to)
   } catch (err) {
     console.error('❌ Erreur envoi email:', err.message)
     throw err
